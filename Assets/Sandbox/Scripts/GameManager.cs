74386f9d8627bc57362b40d33cd225e8fb39bce3
Takuguchi <shankouta548@gmail.com>
2026-05-23 22:43:11 +0900

    カードデータのサンプルを受け取る処理を追加（#1）

--- Assets/Sandbox/Scripts/GameManager.cs
+++ Assets/Sandbox/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // 手札にカードを生成
 
     [SerializeField] Transform playerHandTransform; // プレイヤーの手札のTransformを取得
-    [SerializeField] GameObject cardPrefab; // カードのPrefabを取得
+    [SerializeField] CardController cardPrefab; // カードのPrefabをCardController型として取得
     void Start()
     {
         CreateCard(playerHandTransform); // プレイヤーの手札にカードを生成
@@ -17,8 +17,9 @@ public class GameManager : MonoBehaviour
     // カードを生成するメソッド
     void CreateCard(Transform hand)
     {
-        // カードのPrefabをインスタンス・親要素に任意のTransformを指定
-        Instantiate(cardPrefab, hand, false);
+        // カードのPrefabをCardController型としてインスタンス(生成)・親要素に任意のTransformを指定
+        CardController card = Instantiate(cardPrefab, hand, false);
+        card.Init();    // CardControllerクラスのInit()メソッドを呼び出す→任意のカードデータの各種変数を取得
     }
 
 }
