7256576038521d118543e3bdaf30717c130a1aa2
Takuguchi <shankouta548@gmail.com>
2026-05-23 20:59:38 +0900

    カード自身を扱うCardControllerクラスの新規作成（#1）

---
+++ Assets/Sandbox/Scripts/CardController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// カード自身を扱うためのコード
+// CardPrefabにアタッチ
+public class CardController : MonoBehaviour
+{
+    // カードの見かけ（view）に関することを操作
+    // カードのデータ（model）に関することを操作
+    
+}
