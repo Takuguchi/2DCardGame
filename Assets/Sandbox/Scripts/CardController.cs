73ad4690924dc9a644be55975962bc3d3ac8bc06
Takuguchi <shankouta548@gmail.com>
2026-05-23 21:25:12 +0900

    Revert "CardController に Init メソッドと CardModel の変数を追加"
    This reverts commit 6b2a4ed33ce6b931340cad429513c5ae5ad59597.

--- Assets/Sandbox/Scripts/CardController.cs
+++ Assets/Sandbox/Scripts/CardController.cs
@@ -9,12 +9,4 @@ public class CardController : MonoBehaviour
     // カードの見かけ（view）に関することを操作
     // カードのデータ（model）に関することを操作
     
-    CardModel model;    // カードデータを格納する変数
-
-    // カードを生成したときに呼ばれるメソッド
-    public void Init(CardModel model)
-    {
-        model = new CardModel();    // カードデータインスタンスを生成
-    }
-    
 }
