e81161dc0beb9384d71585fd08a360de51283d35
Takuguchi <shankouta548@gmail.com>
2026-05-23 21:13:11 +0900

    カードデータの各種変数の定義を完了（#1）

--- Assets/Sandbox/Scripts/CardModel.cs
+++ Assets/Sandbox/Scripts/CardModel.cs
@@ -5,5 +5,10 @@ using UnityEngine;
 // カードデータそのものとその処理
 public class CardModel
 {
-    
+    // カードデータの各種変数を定義
+    public string name; // カードの名前
+    public int hp;      // カードのHP
+    public int at;      // カードの攻撃力
+    public int cost;    // カードのコスト
+    public Sprite icon; // カードの絵柄
 }
