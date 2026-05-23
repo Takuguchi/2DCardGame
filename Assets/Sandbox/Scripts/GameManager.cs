1ca0ec1aec0703bc5795fbada59ba3d07319b12a
Takuguchi <shankouta548@gmail.com>
2026-05-23 18:58:04 +0900

    プレイヤーの手札にカードを生成する処理の追加を完了

--- Assets/Sandbox/Scripts/GameManager.cs
+++ Assets/Sandbox/Scripts/GameManager.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    // 手札にカードを生成
+
+    [SerializeField] Transform playerHandTransform; // プレイヤーの手札のTransformを取得
+    [SerializeField] GameObject cardPrefab; // カードのPrefabを取得
     void Start()
     {
+        CreateCard(playerHandTransform); // プレイヤーの手札にカードを生成
+        
+    }
 
+    // カードを生成するメソッド
+    void CreateCard(Transform hand)
+    {
+        // カードのPrefabをインスタンス・親要素に任意のTransformを指定
+        Instantiate(cardPrefab, hand, false);
     }
 
 }
