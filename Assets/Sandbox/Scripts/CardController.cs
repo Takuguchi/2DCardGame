5cbe199ed5e4cd2a93c2f85096acef7a473e3149
Takuguchi <shankouta548@gmail.com>
2026-05-23 21:34:59 +0900

    カード生成時にカードデータを生成するメソッドを定義（#1）

--- Assets/Sandbox/Scripts/CardController.cs
+++ Assets/Sandbox/Scripts/CardController.cs
@@ -8,5 +8,13 @@ public class CardController : MonoBehaviour
 {
     // カードの見かけ（view）に関することを操作
     // カードのデータ（model）に関することを操作
+
+    CardModel model; // カードのデータを格納する変数
+
+    // カードを生成したときに呼ばれるメソッド
+    public void Init()
+    {
+        model = new CardModel(); // カードのデータを生成
+    }
     
 }
