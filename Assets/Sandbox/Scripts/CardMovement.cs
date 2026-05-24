1ba2c828b26afc07414a359950d0c0f0244c3405
Takuguchi <shankouta548@gmail.com>
2026-05-24 14:47:10 +0900

    カードのドラッグ終了時に、ドラッグ開始時の位置に戻る処理を追加（#3）

--- Assets/Sandbox/Scripts/CardMovement.cs
+++ Assets/Sandbox/Scripts/CardMovement.cs
@@ -5,12 +5,31 @@ using UnityEngine.EventSystems;
 
 // カードのPrefabにアタッチ
 // カード側の動きを制御するクラス
-public class CardMovement : MonoBehaviour, IDragHandler
+public class CardMovement : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    // カードをドラッグしたときに呼び出されるメソッド
+    Transform defaultParent; // カードの親の位置を保存する変数
+    
+    // ドラッグ開始時に呼び出されるメソッド
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // 自分自身の親を取得（初期位置が手札なら親は手札）
+        defaultParent = transform.parent;
+
+        // 親の親を、親に設定→カードの移動がスムーズになる
+        transform.SetParent(defaultParent.parent, false);
+    }
+    
+    // カードをドラッグしている最中に呼び出されるメソッド
     public void OnDrag(PointerEventData eventData)
     {
-        // 左辺：カードの場所　右辺：マウスの場所
+        // 左辺：カードの位置　右辺：マウスの位置
         transform.position = eventData.position;
+    }
+
+    // ドラッグ終了時(離したとき)に呼び出されるメソッド
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // カードの親をドラッグ開始時の親に設定→手札に戻る
+        transform.SetParent(defaultParent, false);
     }
 }
