d674362469d26642ff7e6dd2e926b5899291bd40
Takuguchi <shankouta548@gmail.com>
2026-05-24 12:53:54 +0900

    カードのデータを見た目に反映する処理を追加（#1）

--- Assets/Sandbox/Scripts/CardController.cs
+++ Assets/Sandbox/Scripts/CardController.cs
@@ -6,15 +6,20 @@ using UnityEngine;
 // CardPrefabにアタッチ
 public class CardController : MonoBehaviour
 {
-    // カードの見かけ（view）に関することを操作
-    // カードのデータ（model）に関することを操作
+    CardView view;  // カードの見た目（view）に関することを操作
+    CardModel model;    // カードのデータ（model）に関することを操作
 
-    CardModel model; // カードのデータを格納する変数
+    void Awake()
+    {
+        view = GetComponent<CardView>();  // カードの見た目（view）のデータをCardViewコンポーネントから取得
+    }
+
 
     // カードを生成したときに呼ばれるメソッド
     public void Init(int cardID)
     {
         model = new CardModel(cardID); // カードのデータを生成
+        view.Show(model); // CardViewクラス内の、データをカードの見た目に反映するShow()メソッドにカードのデータを渡す
     }
     
 }
