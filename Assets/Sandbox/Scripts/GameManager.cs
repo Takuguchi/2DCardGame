8bb03a3b9e8f3ed2ba7b2ef3c83d88873488452b
Takuguchi <shankouta548@gmail.com>
2026-06-25 16:11:49 +0900

    敵がカードを出したらマナコストを消費する処理を追加（#14）

--- Assets/Sandbox/Scripts/GameManager.cs
+++ Assets/Sandbox/Scripts/GameManager.cs
@@ -213,6 +213,9 @@ public class GameManager : MonoBehaviour
             CardController enemyCard = selectableHandCardList[0]; // とりあえずカードリストの一番最初のカードを選択
             // カードを移動
             enemyCard.movement.SetCardTransform(enemyFieldTransform); // カードの移動を行うCardMovementクラスのSetCardTransform()メソッドに、カードの移動先のTransformを渡す
+            ReduceManaCost(enemyCard.model.cost, false); // カードを出したら敵のManaコストを減らす　引数isPlayerCardはfalseで渡す
+            enemyCard.model.isFieldCard = true; // カードを出したらフィールドのカードにする
+
         }
 
         /* 攻撃 */
